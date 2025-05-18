4de6da09ef868dc4762fe3a0ce27387cb4f10968
martuniukanastasia <martuniuk.anastasia@gmail.com>
2025-05-19 00:35:01 +0300

    Add files via upload

---
+++ MyForm.h
@@ -0,0 +1,179 @@
+﻿#pragma once
+#include<cmath>
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Panel^ panel2;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+
+	protected:
+
+
+
+
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->panel2->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::SystemColors::MenuBar;
+			this->panel2->Controls->Add(this->textBox2);
+			this->panel2->Controls->Add(this->textBox3);
+			this->panel2->Controls->Add(this->button1);
+			this->panel2->Controls->Add(this->label3);
+			this->panel2->Controls->Add(this->textBox1);
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->label2);
+			this->panel2->Location = System::Drawing::Point(105, 58);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(355, 232);
+			this->panel2->TabIndex = 7;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(46, 49);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(25, 20);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"у=";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(46, 17);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(25, 20);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"х=";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(75, 14);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(131, 26);
+			this->textBox1->TabIndex = 3;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(8, 113);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(181, 20);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Заокруглене значення";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(212, 26);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(122, 33);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Обчислити";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(195, 110);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(139, 26);
+			this->textBox3->TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(75, 49);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(131, 26);
+			this->textBox2->TabIndex = 4;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ControlLight;
+			this->ClientSize = System::Drawing::Size(743, 413);
+			this->Controls->Add(this->panel2);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		double x, y;
+		int a;
+		x = Convert::ToDouble(textBox1->Text);
+		y = sqrt(fabs(x - 1) + sin(x));
+		textBox2->Text = Convert::ToString(y);
+		a = ceil(y);
+		textBox3->Text = Convert::ToString(a);
+	}
+};
+}
